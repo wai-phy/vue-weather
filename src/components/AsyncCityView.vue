abe74d80d2fcd1167003b9d70dd0e4318aceafa6
Wai Phyo Aung <waefioong@gmail.com>
2024-02-22 23:50:47 +0630

    Fer Degree to Celsius

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -113,7 +113,7 @@ const route = useRoute();
 const getWeatherData = async () => {
     try {
         // const weatherData = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial
-        const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial
+        const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=metric
 `);
         // cal current date & time
         const localOffset = new Date().getTimezoneOffset() * 60000;
